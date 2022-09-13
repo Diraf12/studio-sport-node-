0d66264c18baf52f2ecc2eb0567101c964b9be44
Diraf12 <104466448+Diraf12@users.noreply.github.com>
2022-09-13 10:14:48 +0200

    maj

--- main.js
+++ main.js
@@ -4,24 +4,23 @@ const port = 3000
 
 app.use(express.static("public"))
 
-app.use(function (req, res, next) {
-    console.log('Time:', Date().toString());
-    next();
-});  
-app.get('/home', function (req, res) {
-  res.status(200).sendFile('/html/index.html', {root:__dirname})
+app.set('view engine', 'ejs')
+
+app.get('/home', (req, res) => {
+    const liste =[ {title: 'bla', bla: 'bla'}, {title: 'bla', bla: 'bla'}]
+  res.status(200).render('index.ejs',{liste})
 })
-app.get('/presatation', function (req, res) {
-  res.status(200).sendFile('/html/presentation.html', {root:__dirname})
+app.get('/presentation', function (req, res) {
+  res.status(200).render('presentation.ejs')
 })
 app.get('/contact', function (req, res) {
-  res.status(200).sendFile('/html/contact.html', {root:__dirname})
+  res.status(200).render('contact.ejs')
 })
 app.get('/', function (req, res) {
   res.status(301).redirect('/home')
 })
 app.use((req, res) => {
-  res.status(404).sendFile('/html/404.html', {root:__dirname})
+  res.status(404).render('404.ejs')
 })
 
 app.listen(port, () => {
